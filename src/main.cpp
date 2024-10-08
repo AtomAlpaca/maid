121e437d247ead4f2adc83f2d06ccb5d9eefbc07
AtomAlpaca <atal@anche.no>
2024-10-08 18:33:21 +0800

    add reply

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,56 @@
 #include "duel.h"
+#include <cstddef>
 #include <cstdint>
 #include <iostream>
 #include <random>
 #include <string>
 #include <tgbot/tgbot.h>
 #include <tgbot/tools/StringTools.h>
+#include <utility>
+#include <vector>
 
 int main()
 {
     auto seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::mt19937 rnd(seed);
     std::string token("");
+    std::vector<std::string> lazy =
+    {
+        "我想睡觉",
+        "呜",
+        "呜呜",
+        "呜呜呜",
+        "呜呜呜呜",
+        "呜呜呜呜呜",
+        "好累",
+        "好困",
+        "肚子疼",
+        "哭哭"
+    };
+    std::vector<TgBot::BotCommand::Ptr> commands;
+    TgBot::BotCommand::Ptr cmdArray(new TgBot::BotCommand);
+    std::vector<std::pair<std::string, std::string>> vec =
+    {
+        {"bind", "绑定 Codeforces 帐号"},
+        {"unbind", "解绑 Codeforces 帐号"},
+        {"name", "获取当前绑定的 Codeforces 帐号"},
+        {"random", "获取随机的 Codeforces 题目"},
+        {"duel", "向对方发起决斗请求"},
+        {"accept", "接受对方的决斗请求"},
+        {"reject", "拒绝决斗请求"},
+        {"done", "完成决斗"}
+    };
+    for (auto & [i, j] : vec)
+    {
+        cmdArray = TgBot::BotCommand::Ptr(new TgBot::BotCommand);
+        cmdArray -> command = i;
+        cmdArray -> description = j;
+        commands.push_back(cmdArray);
+    }
 
     TgBot::Bot bot(token);
     duel::Duel duel;
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "呜呜");
-    });
-
+    bot.getApi().setMyCommands(commands);
     bot.getEvents().onCommand("bind", [&](TgBot::Message::Ptr message) {
         auto vec = StringTools::split(message -> text, ' ');
         if (vec.size() != 2)
@@ -225,6 +258,60 @@ int main()
         else
         {
             bot.getApi().sendMessage(message -> chat -> id, "你还未通过对应的题目！", nullptr, ptr);
+        }
+    });
+
+    bot.getEvents().onAnyMessage([&](TgBot::Message::Ptr message) {
+        auto ptr = std::make_shared<TgBot::ReplyParameters>();
+        ptr -> messageId = message -> messageId;
+        ptr -> chatId    = message -> chat -> id;
+        ptr -> allowSendingWithoutReply = true;
+        if (!StringTools::startsWith(message->text, "/")) {
+            return;
+        }
+        if (std::uniform_int_distribution<int64_t>(0, 99)(rnd) == 0)
+        {
+            bot.getApi().sendMessage(message -> chat -> id, lazy[std::uniform_int_distribution<int64_t>(0, lazy.size() - 1)(rnd)]);
+        }
+        for (auto & [i, j] : vec)
+        {
+            if (StringTools::startsWith(message->text, std::string("/") + i))
+            {
+                return ;
+            }
+        }
+        auto v = StringTools::split(message -> text, ' ');
+        v[0].erase(v[0].begin());
+        if (v.size() == 1)
+        {
+            if (message -> replyToMessage == nullptr)
+            {
+                bot.getApi().sendMessage(message -> chat -> id,
+                    message -> from -> firstName + message -> from -> lastName + " " + v[0] + "了自己！",
+                    nullptr, ptr);
+            }
+            else
+            {
+                bot.getApi().sendMessage(message -> chat -> id,
+                    message -> from -> firstName + message -> from -> lastName + " " + v[0] + "了 "
+                    + message -> replyToMessage -> from -> firstName + message -> replyToMessage -> from -> lastName + "！",
+                    nullptr, ptr);
+            }
+        }
+        else
+        {
+            if (message -> replyToMessage == nullptr)
+            {
+                bot.getApi().sendMessage(message -> chat -> id,
+                    message -> from -> firstName + message -> from -> lastName + " " + v[0] + "自己" + v[1] + "！",
+                    nullptr, ptr);
+            }
+            else
+            {
+                bot.getApi().sendMessage(message -> chat -> id, message -> from -> firstName + message -> from -> lastName + " " + v[0] +
+                    message -> replyToMessage -> from -> firstName + message -> replyToMessage -> from -> lastName + v[1] + "！",
+                    nullptr, ptr);
+            }
         }
     });
 
